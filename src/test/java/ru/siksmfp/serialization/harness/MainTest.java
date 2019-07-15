3f56917d0c5d6743dd24c4059496a0196f1c24f7
parkito <artyom-karnov@yandex.ru>
2019-07-15 19:57:48 +0300

    Add first JMH test

--- src/test/java/ru/siksmfp/serialization/harness/MainTest.java
+++ src/test/java/ru/siksmfp/serialization/harness/MainTest.java
@@ -1,5 +1,15 @@
 package ru.siksmfp.serialization.harness;
 
-class MainTest {
+import org.openjdk.jmh.annotations.Benchmark;
 
+public class MainTest {
+
+    @Benchmark
+    public void test() {
+        System.out.println(Math.sqrt(10023.1));
+    }
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
 }
