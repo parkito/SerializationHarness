7950ab5038427aa6831371a8cf3e07ce68762f78
parkito <artyom-karnov@yandex.ru>
2019-07-16 15:43:22 +0300

    Add simple benchmark results

--- src/main/java/ru/siksmfp/serialization/harness/banchmark/SimpleBenchmark.java
+++ src/main/java/ru/siksmfp/serialization/harness/banchmark/SimpleBenchmark.java
@@ -31,12 +31,24 @@ public class SimpleBenchmark extends ParentBenchmark {
 
 
     @Benchmark
-    public boolean findArrayListBenchmark(ListState arrayListState) {
-        return arrayListState.arrayList.contains("256");
+    public String findArrayListBenchmark(ListState listState) {
+        return listState.arrayList.get(900);
     }
 
     @Benchmark
-    public boolean findLinkedListBenchmark(ListState arrayListState) {
-        return arrayListState.arrayList.contains("256");
+    public String findLinkedListBenchmark(ListState listState) {
+        return listState.linkedList.get(900);
     }
+
+    //java 8
+
+//    Benchmark                                Mode  Cnt    Score   Error  Units
+//    SimpleBenchmark.findArrayListBenchmark   avgt   10    3.068 ± 0.066  ns/op
+//    SimpleBenchmark.findLinkedListBenchmark  avgt   10  113.641 ± 1.518  ns/op
+
+    //java 11
+
+//    Benchmark                                Mode  Cnt    Score   Error  Units
+//    SimpleBenchmark.findArrayListBenchmark   avgt   10    3.267 ± 0.172  ns/op
+//    SimpleBenchmark.findLinkedListBenchmark  avgt   10  114.343 ± 1.283  ns/op
 }
