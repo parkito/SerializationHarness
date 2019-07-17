0fc31312152a44eccec8140ab5b41fc2721e91a0
parkito <artyom-karnov@yandex.ru>
2019-07-18 02:46:47 +0300

    Move simple benchmark to tests

--- src/test/java/ru/siksmfp/serialization/harness/SimpleBenchmark.java
+++ src/test/java/ru/siksmfp/serialization/harness/SimpleBenchmark.java
@@ -1,8 +1,21 @@
-package ru.siksmfp.serialization.harness.banchmark.impl;
+package ru.siksmfp.serialization.harness;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
 import ru.siksmfp.serialization.harness.state.impl.ListState;
 
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Measurement(iterations = 5)
+@Warmup(iterations = 5)
 public class SimpleBenchmark {
 
     @Benchmark
