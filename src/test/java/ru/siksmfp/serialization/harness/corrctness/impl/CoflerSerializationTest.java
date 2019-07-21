c7d1f2d6434adc6e5628130f2d7f541dbbb91a0a
parkito <artyom-karnov@yandex.ru>
2019-07-21 13:19:52 +0300

    Include all tests in maven test phase

--- src/test/java/ru/siksmfp/serialization/harness/corrctness/impl/CoflerSerializationTest.java
+++ src/test/java/ru/siksmfp/serialization/harness/corrctness/impl/CoflerSerializationTest.java
@@ -4,6 +4,7 @@ import ru.siksmfp.serialization.harness.corrctness.api.ParentSerializationTest;
 import ru.siksmfp.serialization.harness.serializer.impl.CoflerSerializer;
 import ru.siksmfp.serialization.harness.state.impl.CoflerUserState;
 
+
 public class CoflerSerializationTest extends ParentSerializationTest {
 
     CoflerSerializationTest() {
