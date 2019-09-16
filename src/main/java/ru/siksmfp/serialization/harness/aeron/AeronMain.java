c28c2368701855946327782d2d129a5adcb3e86b
parkito <artyom-karnov@yandex.ru>
2019-09-15 22:20:55 -0600

    Remove aeron client

--- src/main/java/ru/siksmfp/serialization/harness/aeron/AeronMain.java
+++
@@ -1,8 +0,0 @@
-package ru.siksmfp.serialization.harness.aeron;
-
-public class AeronMain {
-
-    public static void main(String[] args) {
-        AeronContext context = new AeronContext(1, "aeron:udp?endpoint=localhost:40123");
-    }
-}
